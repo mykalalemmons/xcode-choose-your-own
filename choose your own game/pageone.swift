7c7a49fd36baae32a95764f309b05aa34054c015
Mykala Lemmons <mykalalemmons@mykalas-macbook-pro-3.local>
2019-02-21 15:09:32 -0600

    initial commit in project

--- choose your own game/pageone.swift
+++ choose your own game/pageone.swift
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var playerName: UITextField!
+class pageone: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
 }
 
